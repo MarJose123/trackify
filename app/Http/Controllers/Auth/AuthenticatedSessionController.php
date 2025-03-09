d20aa0922266023d6444efbaca46dc7e25f3442d
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-03-09 11:39:39 +0800

    ♻️ refactor(`AuthenticatedSessionController.php`): Redirect to login route after logout.

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -51,6 +51,6 @@ class AuthenticatedSessionController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        return redirect()->route('login');
     }
 }
