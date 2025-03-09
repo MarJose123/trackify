0e8ea045a8601dbdfe1173ece7b916610b1f8ad4
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-03-09 11:19:43 +0800

    ✨ feat(App): Implement first-time installation check.  Adds a check to redirect to registration if no users exist.

---
+++ app/Concerns/Application.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Concerns;
+
+use App\Models\User;
+
+trait Application
+{
+
+    public function isFirstInstalled(): bool
+    {
+        // confirming by having no user available
+        return User::count() === 0;
+    }
+
+}
--- app/Providers/NativeAppServiceProvider.php
+++ app/Providers/NativeAppServiceProvider.php
@@ -2,14 +2,18 @@
 
 namespace App\Providers;
 
+use App\Concerns\Application;
 use App\Enums\DefaultWindowSize;
 use App\Enums\WindowName;
+use App\Models\User;
 use Native\Laravel\Contracts\ProvidesPhpIni;
 use Native\Laravel\Facades\Menu;
 use Native\Laravel\Facades\Window;
 
 class NativeAppServiceProvider implements ProvidesPhpIni
 {
+    use Application;
+
     /**
      * Executed once the native application has been booted.
      * Use this method to open windows, register global shortcuts, etc.
@@ -23,7 +27,10 @@ class NativeAppServiceProvider implements ProvidesPhpIni
             ->position(100, 100)
             ->minHeight(DefaultWindowSize::HEIGHT->getSize())
             ->minWidth(DefaultWindowSize::WIDTH->getSize())
+            ->url($this->isFirstInstalled() ? route('register') : route('login'))
             ->rememberState();
+
+
     }
 
     /**
