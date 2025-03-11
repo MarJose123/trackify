4f7661e4744231b027a6a0ff3359be81fb975ee7
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-03-11 19:06:55 +0800

    ➕ refactor(composer.json & composer.lock): Add new dependencies.  Updated `composer.json` and `composer.lock` files with new packages.

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@ Route::middleware('auth')->get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->name('dashboard');
 
+Route::get('/test', function () {
+    echo 'test';
+   ray('test');
+});
+
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
 require __DIR__.'/clients.php';
