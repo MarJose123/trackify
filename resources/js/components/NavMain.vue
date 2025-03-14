571ba61593eed275c1b4ed751143ff0d31f9cbef
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-03-14 17:47:27 +0800

    ♻️ refactor(`NavMain.vue`): Improve navigation logic to handle partial URL matches.

--- resources/js/components/NavMain.vue
+++ resources/js/components/NavMain.vue
@@ -15,7 +15,7 @@ const page = usePage<SharedData>();
         <SidebarGroupLabel>Application</SidebarGroupLabel>
         <SidebarMenu>
             <SidebarMenuItem v-for="item in items" :key="item.title">
-                <SidebarMenuButton as-child :is-active="item.href === page.url">
+                <SidebarMenuButton as-child :is-active="item.href === page.url || page.url.startsWith(item.href)">
                     <Link :href="item.href">
                         <component :is="item.icon" />
                         <span>{{ item.title }}</span>
