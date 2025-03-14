653a291b6c3e177b0cdb9de238b1d4bd10a3a5c8
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-03-14 17:42:07 +0800

    ✨ feat(routes/clients.php): Add new routes for clients management and API endpoint for filtering.

--- routes/clients.php
+++ routes/clients.php
@@ -3,6 +3,12 @@
 use App\Http\Controllers\Clients\ClientsController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth'])->group(function () {
-    Route::get('clients', [ClientsController::class, 'create'])->name('clients.create');
+Route::middleware(['auth'])->prefix('clients')->group(function () {
+    Route::get('/', [ClientsController::class, 'list'])->name('clients.list');
+    Route::get('/create', [ClientsController::class, 'create'])->name('clients.create');
+
+    // axios route
+    Route::prefix('_api')->group(function () {
+        Route::get('/table-filter-status', [ClientsController::class, 'tableFilterStatus'])->name('clients.table-filter-status');
+    });
 });
