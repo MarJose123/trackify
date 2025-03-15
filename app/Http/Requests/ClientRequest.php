d7c274bd2a1b97ad49285069eb72b859c442f1cb
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-03-15 17:14:19 +0800

    ✨ feat(ClientRequest): Add validation rules for company name, currency, billing method, status, and rate.

--- app/Http/Requests/ClientRequest.php
+++ app/Http/Requests/ClientRequest.php
@@ -9,7 +9,12 @@ class ClientRequest extends FormRequest
     public function rules(): array
     {
         return [
+            'company_name' => ['required', 'unique:clients,company_name'],
             'name' => ['required', 'string'],
+            'currency' => ['required'],
+            'billing_method' => ['required'],
+            'status' => ['required'],
+            'rate' => ['required', 'numeric'],
         ];
     }
 
