2cd09a9cf1d09c12871cae7e1a6eb31e79d7fcc7
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-03-15 16:49:23 +0800

    ✨ feat(ClientsController): Add client creation functionality.

--- app/Http/Controllers/Clients/ClientsController.php
+++ app/Http/Controllers/Clients/ClientsController.php
@@ -6,6 +6,7 @@ use App\Enums\BillingMethod;
 use App\Enums\CurrencyCode;
 use App\Enums\Status;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ClientRequest;
 use App\Models\Clients;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -15,7 +16,14 @@ class ClientsController extends Controller
 {
     public function list(Request $request): Response
     {
-        $clients = Clients::paginate(200);
+        $request->validate([
+            /** @query */
+            'per_page' => 'nullable|integer|min:15|max:200',
+        ]);
+
+        $perPage = $request->query('per_page', 200);
+
+        $clients = Clients::query()->paginate(perPage: $perPage);
 
         return Inertia::render('clients/List', [
             'clients' => $clients,
@@ -24,13 +32,27 @@ class ClientsController extends Controller
 
     public function create()
     {
-        return Inertia::render('clients/Create',[
+        return Inertia::render('clients/Create', [
             'fields' => [
                 'status' => Status::cases(),
                 'currency' => CurrencyCode::cases(),
                 'billing_method' => BillingMethod::cases(),
-            ]
+            ],
         ]);
+    }
+
+    public function store(ClientRequest $request)
+    {
+        Clients::create([
+            'user_id' => $request->user()->id,
+            ...$request->validated(),
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Client created',
+            'description' => sprintf('Company Name %s has been saved.', $request->company_name),
+        ], \Symfony\Component\HttpFoundation\Response::HTTP_CREATED);
     }
 
     public function tableFilterStatus()
