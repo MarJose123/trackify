bd975a90fe35c24638c4dc0d0e6ded43567b748a
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-03-15 17:14:28 +0800

    ✨ feat(resources/js/pages/clients/Create.vue): Enhance client creation form.
    The form now includes validation, improved UI, and API integration for submission.

--- resources/js/pages/clients/Create.vue
+++ resources/js/pages/clients/Create.vue
@@ -1,32 +1,21 @@
 <script setup lang="ts">
+import { Button } from '@/components/ui/button';
+import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { NumberField, NumberFieldContent, NumberFieldInput } from '@/components/ui/number-field';
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { ToastAction, useToast } from '@/components/ui/toast';
 import AppLayout from '@/layouts/AppLayout.vue';
+import { api } from '@/lib/axios';
 import { type BreadcrumbItem } from '@/types';
-import { Head, usePage } from '@inertiajs/vue3';
-import { toTypedSchema } from '@vee-validate/zod'
-import * as z from 'zod'
-import { useForm } from 'vee-validate'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
 import { CreationSharedData } from '@/types/clients';
-import { Loader2 } from 'lucide-vue-next'
-import {
-    Select,
-    SelectContent,
-    SelectGroup,
-    SelectItem,
-    SelectLabel,
-    SelectTrigger,
-    SelectValue,
-} from '@/components/ui/select'
-import {
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from '@/components/ui/form'
-import {Button} from '@/components/ui/button';
+import { Head, usePage } from '@inertiajs/vue3';
+import { toTypedSchema } from '@vee-validate/zod';
+import { Loader2 } from 'lucide-vue-next';
+import { useForm } from 'vee-validate';
+import { h } from 'vue';
+import { toast } from 'vue-sonner';
+import * as z from 'zod';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -39,24 +28,89 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
-const formSchema = toTypedSchema(z.object({
-    company_name: z.string().min(2).max(225),
-    name: z.string().min(2).max(225),
-    status: z.string().min(2).max(225)
-}))
+const formSchema = toTypedSchema(
+    z.object({
+        company_name: z
+            .string({
+                required_error: 'This field required to have a value.',
+            })
+            .min(2)
+            .max(225),
+        name: z
+            .string({
+                required_error: 'This field required to have a value.',
+            })
+            .min(2)
+            .max(225),
+        status: z
+            .string({
+                required_error: 'This field required to have a value.',
+            })
+            .min(2)
+            .max(225),
+        billing_method: z.string({
+            required_error: 'This field required to have a value.',
+        }),
+        currency: z.string({
+            required_error: 'This field required to have a value.',
+        }),
+        rate: z.coerce
+            .number({
+                required_error: 'This field required to have a value.',
+                invalid_type_error: 'Field value must be a number',
+            })
+            .positive(),
+    }),
+);
 
-const { handleSubmit, isSubmitting, isFieldDirty } = useForm({
+const { handleSubmit, isSubmitting, isFieldDirty, setFieldValue, resetForm } = useForm({
     validationSchema: formSchema,
-})
+    initialValues: {
+        rate: 500,
+        status: 'Active',
+    },
+});
 
-const onSubmit = handleSubmit((values) => {
-    setInterval(() => console.log('release'),500000 )
-    console.log('Form submitted!', values)
-})
+const onSubmit = handleSubmit(async (values) => {
+    await api()
+        .post(route('clients.store'), values)
+        .then((resp) => {
+            if (resp.status === 201) {
+                toast(resp.data.message, {
+                    description: resp.data.description,
+                });
+                resetForm();
+            }
+        })
+        .catch((resp) => {
+            console.log(resp);
+            if (resp.status === 422) {
+                useToast().toast({
+                    title: 'Uh oh! Something went wrong.',
+                    description: resp.response.data.message,
+                    variant: 'destructive',
+                });
+            } else {
+                useToast().toast({
+                    title: 'Uh oh! Something went wrong.',
+                    description: 'There was a problem with your request.',
+                    variant: 'destructive',
+                    action: h(
+                        ToastAction,
+                        {
+                            altText: 'Try again',
+                            onClick: onSubmit,
+                        },
+                        {
+                            default: () => 'Try again',
+                        },
+                    ),
+                });
+            }
+        });
+});
 
-const page = usePage<CreationSharedData>()
-
-
+const page = usePage<CreationSharedData>();
 </script>
 
 <template>
@@ -65,13 +119,13 @@ const page = usePage<CreationSharedData>()
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl px-4 py-6">
             <form @submit="onSubmit" class="grid gap-y-5">
-                <div class="grid auto-rows-min gap-3 md:grid-cols-2 gap-y-4">
+                <div class="grid auto-rows-min gap-3 gap-y-4 md:grid-cols-2">
                     <div class="grid w-full max-w-sm items-center gap-1.5">
-                        <FormField name="company_name"  v-slot="{ componentField }" :validate-on-blur="!isFieldDirty">
+                        <FormField name="company_name" v-slot="{ componentField }" :validate-on-blur="!isFieldDirty">
                             <FormItem>
                                 <FormLabel>Company Name</FormLabel>
                                 <FormControl>
-                                    <Input id="company_name" type="text" v-bind="componentField"  />
+                                    <Input id="company_name" type="text" v-bind="componentField" />
                                 </FormControl>
                                 <FormDescription />
                                 <FormMessage />
@@ -83,7 +137,7 @@ const page = usePage<CreationSharedData>()
                             <FormItem>
                                 <FormLabel>Contact Name</FormLabel>
                                 <FormControl>
-                                    <Input id="name" type="text" v-bind="componentField"/>
+                                    <Input id="name" type="text" v-bind="componentField" />
                                 </FormControl>
                                 <FormDescription />
                                 <FormMessage />
@@ -115,46 +169,85 @@ const page = usePage<CreationSharedData>()
                         </FormField>
                     </div>
                     <div class="grid w-full max-w-sm items-center gap-1.5">
-                        <Label for="name">Billing Method</Label>
-                        <Select class="flex w-full">
-                            <SelectTrigger class="w-full">
-                                <SelectValue placeholder="Select Billing Method" />
-                            </SelectTrigger>
-                            <SelectContent>
-                                <SelectGroup>
-                                    <SelectLabel>Billing Method</SelectLabel>
-                                    <SelectItem v-for="method in page?.props.fields.billing_method" :value="method" :key="method">
-                                        {{ method }}
-                                    </SelectItem>
-                                </SelectGroup>
-                            </SelectContent>
-                        </Select>
+                        <FormField name="billing_method" v-slot="{ componentField }" :validate-on-blur="!isFieldDirty">
+                            <FormItem>
+                                <FormLabel>Billing Method</FormLabel>
+                                <FormControl>
+                                    <Select class="flex w-full" v-bind="componentField">
+                                        <SelectTrigger class="w-full">
+                                            <SelectValue placeholder="Select Billing Method" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            <SelectGroup>
+                                                <SelectLabel>Billing Method</SelectLabel>
+                                                <SelectItem v-for="method in page?.props.fields.billing_method" :value="method" :key="method">
+                                                    {{ method }}
+                                                </SelectItem>
+                                            </SelectGroup>
+                                        </SelectContent>
+                                    </Select>
+                                </FormControl>
+                                <FormDescription />
+                                <FormMessage />
+                            </FormItem>
+                        </FormField>
                     </div>
                     <div class="grid w-full max-w-sm items-center gap-1.5">
-                        <Label for="name">Currency</Label>
-                        <Select class="flex w-full">
-                            <SelectTrigger class="w-full">
-                                <SelectValue placeholder="Select Currency" />
-                            </SelectTrigger>
-                            <SelectContent>
-                                <SelectGroup>
-                                    <SelectLabel>Currency</SelectLabel>
-                                    <SelectItem v-for="currency in page?.props.fields.currency" :value="currency" :key="currency">
-                                        {{ currency }}
-                                    </SelectItem>
-                                </SelectGroup>
-                            </SelectContent>
-                        </Select>
+                        <FormField name="currency" v-slot="{ componentField }" :validate-on-blur="!isFieldDirty">
+                            <FormItem>
+                                <FormLabel>Currency</FormLabel>
+                                <FormControl>
+                                    <Select class="flex w-full" v-bind="componentField">
+                                        <SelectTrigger class="w-full">
+                                            <SelectValue placeholder="Select Currency" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            <SelectGroup>
+                                                <SelectLabel>Currency</SelectLabel>
+                                                <SelectItem v-for="currency in page?.props.fields.currency" :value="currency" :key="currency">
+                                                    {{ currency }}
+                                                </SelectItem>
+                                            </SelectGroup>
+                                        </SelectContent>
+                                    </Select>
+                                </FormControl>
+                                <FormDescription />
+                                <FormMessage />
+                            </FormItem>
+                        </FormField>
                     </div>
                     <div class="grid w-full max-w-sm items-center gap-1.5">
-                        <Label for="salary">Rate</Label>
-                        <Input id="salary" type="text" />
+                        <FormField name="rate" v-slot="{ value }" :validate-on-blur="!isFieldDirty">
+                            <FormItem>
+                                <FormLabel>Rate</FormLabel>
+                                <FormControl>
+                                    <NumberField
+                                        :model-value="value"
+                                        @update:model-value="
+                                            (v: number | undefined) => {
+                                                if (v && typeof v !== undefined) {
+                                                    setFieldValue('rate', v as number);
+                                                } else {
+                                                    setFieldValue('rate', undefined);
+                                                }
+                                            }
+                                        "
+                                    >
+                                        <NumberFieldContent>
+                                            <NumberFieldInput />
+                                        </NumberFieldContent>
+                                    </NumberField>
+                                </FormControl>
+                                <FormDescription />
+                                <FormMessage />
+                            </FormItem>
+                        </FormField>
                     </div>
                 </div>
-                <div class="flex w-full flex-row-reverse ">
+                <div class="flex w-full flex-row-reverse">
                     <Button type="submit" :disabled="isSubmitting">
-                        <Loader2 v-if="isSubmitting" class="w-4 h-4 mr-2 animate-spin" />
-                        Save
+                        <Loader2 v-if="isSubmitting" class="mr-2 h-4 w-4 animate-spin" />
+                        {{ isSubmitting ? 'Saving..' : 'Save' }}
                     </Button>
                 </div>
             </form>
