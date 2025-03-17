c5f375f25a9a9785c182ca6546237b13c5c9ec34
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-03-17 13:55:44 +0800

    ✨ feat(routes/clients.php): Add routes for editing and updating clients.

--- routes/clients.php
+++ routes/clients.php
@@ -8,6 +8,8 @@ Route::middleware(['auth'])->prefix('clients')->group(function () {
     Route::get('/create', [ClientsController::class, 'create'])->name('clients.create');
     Route::post('/create', [ClientsController::class, 'store'])->name('clients.store');
     Route::get('/show/{client}', [ClientsController::class, 'show'])->name('clients.show');
+    Route::get('/edit/{client}', [ClientsController::class, 'edit'])->name('clients.edit');
+    Route::put('/update/{client}', [ClientsController::class, 'edit'])->name('clients.update');
 
     // axios route
     Route::prefix('_api')->group(function () {
