d62d6542455f2b92ab15f6add69604ca9f0ccfbc
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-03-17 13:33:34 +0800

    ♻️ refactor(`ClientRequest`): Improve validation rules for client creation and updates.  Uses separate rules for POST and other methods.

--- app/Http/Requests/ClientRequest.php
+++ app/Http/Requests/ClientRequest.php
@@ -2,11 +2,30 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Clients;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class ClientRequest extends FormRequest
 {
     public function rules(): array
+    {
+        return $this->isMethod('POST') ? $this->creationValidationRules() : $this->updateValidationRules();
+    }
+
+    protected function updateValidationRules(): array
+    {
+        return [
+            'company_name' => ['required', Rule::unique('clients')->ignore($this->route('client'))],
+            'name' => ['required', 'string'],
+            'currency' => ['required'],
+            'billing_method' => ['required'],
+            'status' => ['required'],
+            'rate' => ['required', 'numeric'],
+        ];
+    }
+
+    protected function creationValidationRules(): array
     {
         return [
             'company_name' => ['required', 'unique:clients,company_name'],
